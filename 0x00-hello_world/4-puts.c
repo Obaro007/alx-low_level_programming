d44ad202925e26d0abee02fe9c90fdc2b65139de
Obaro007 <emamuzoobaro@gmail.com>
2023-06-16 04:43:53 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - A program using the funxtion puts
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
