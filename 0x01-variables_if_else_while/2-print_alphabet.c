80c024a15bbfbfbf51dd06f4d26711eefc1c8f43
Obaro007 <emamuzoobaro@gmail.com>
2023-06-18 08:41:33 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,8 +3,9 @@
 /**
  * main - Entry Point
  *
- * Write a program that prints the alphabet in lowercase
+ * Description: Write a program that prints the alphabet in lowercase
  *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
