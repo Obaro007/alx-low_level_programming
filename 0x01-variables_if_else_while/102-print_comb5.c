05bebe33f9fd651de88ca7072ef10eb0033ede98
Obaro007 <emamuzoobaro@gmail.com>
2023-06-18 11:45:26 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of two two-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 99; i++)
+	{
+		for (j = i+1; j < 100; j++)
+		{
+			putchar('0' + (i / 10));
+			putchar(('0' + i % 10));
+
+			putchar(' ');
+
+			putchar('0' + (j / 10));
+			putchar('0' + (j % 10));
+
+			if (!(i == 98 && j == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
