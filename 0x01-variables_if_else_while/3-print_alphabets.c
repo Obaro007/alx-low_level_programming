ee9b7fdb9387c0ac4194eba6f0065ed8185eede0
Obaro007 <emamuzoobaro@gmail.com>
2023-06-18 09:05:36 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,26 +6,23 @@
  * Description:  A program that prints the alphabet in lowercase and uppercase
  *
  * Return: Always 0 (Success)
- *
  */
 
 int main(void)
 {
 	char c;
 
-
-	for (c='a'; c<='z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 	}
 
-	for (c='A'; c<='Z'; c++)
+	for (c = 'A'; c <= 'Z'; c++)
 	{
 		putchar(c);
 	}
 
-	putchar('/n');
-
+	putchar('\n');
 
 	return (0);
 }
