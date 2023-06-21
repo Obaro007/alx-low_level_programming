5c5edd641e6b2bff9e5e50cc314c518f993b0869
Obaro007 <emamuzoobaro@gmail.com>
2023-06-21 16:38:13 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - It will computes the absolute value of an integer.
+ *
+ * @n: The integer to be converted;
+ *
+ * Return: Returns an absolute/non-negative integer.
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
