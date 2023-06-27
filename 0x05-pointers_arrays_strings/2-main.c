10ae00caf7ac2313b50968ccdfe44640cc984943
Obaro007 <emamuzoobaro@gmail.com>
2023-06-26 21:52:05 -0700

    2-largest_number.c

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -1,18 +1,23 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - prints the largest of 3 integers
+* Return: 0
+*/
+
 int main(void)
 {
-    char *str;
-    int len;
+        int a, b, c;
+        int largest;
 
-    str = "My first strlen!";
-    len = _strlen(str);
-    printf("%d\n", len);
-    return (0);
+        a = 972;
+        b = -98;
+        c = 0;
+
+        largest = largest_number(a, b, c);
+
+        printf("%d is the largest number\n", largest);
+
+        return (0);
 }
