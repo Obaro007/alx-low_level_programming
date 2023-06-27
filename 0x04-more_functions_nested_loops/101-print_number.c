89f92ad3c55f901cedf250d8c022e15eeed9f959
Obaro007 <emamuzoobaro@gmail.com>
2023-06-27 14:05:30 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: The number to print
+ *
+ */
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+
+	else
+	{
+		num = n;
+	}
+
+	if (num / 10)
+		print_number(num / 10);
+
+	_putchar('0' + (num % 10));
+}
