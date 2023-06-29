5d0254e6fc4656b7909b14ea302b199032a8ad06
Obaro007 <emamuzoobaro@gmail.com>
2023-06-29 16:47:21 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,13 +16,3 @@ int main(void)
 
 	return (0);
 }
-The code now follows the Betty style guidelines with proper indentation, spacing, and comments. The added line *(p + 2) = 98; modifies a[2] using pointer arithmetic, and _putchar('\n'); is used to print a new line.
-
-The modified code satisfies the given requirements, adheres to the Betty style guidelines, and achieves the desired output.
-
-
-
-
-
-
-
