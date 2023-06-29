fe07db752c90a87620add209f30e7cdae9e79de8
Obaro007 <emamuzoobaro@gmail.com>
2023-06-29 15:53:01 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+
+/**
+ * _strcmp - The program compares two strings.
+ * @s1: String one to compare
+ * @s2: String two to compare
+ *
+ * Return: An integer from the comparison
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+	{
+		i++;
+	}
+
+	return (s1[i] - s2[i]);
+}
