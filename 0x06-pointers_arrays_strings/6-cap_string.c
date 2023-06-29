9b4a09b2ef4a58f5d819639c51dc08c3de0fd1d1
Obaro007 <emamuzoobaro@gmail.com>
2023-06-29 16:12:27 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * cap_string - The function capitalizes the first character of
+ * all words in a string.
+ * @str: The string to capitalize
+ *
+ * Return: The str, the updated string.
+ */
+char *cap_string(char *str)
+{
+	int next = 1, i;
+	char separators[] = " \t\n,;.!?\"(){}";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (strchr(separators, str[i]) != NULL)
+			next = 1;
+
+		else if (next && isalpha(str[i]))
+		{
+			str[i] = (char)toupper((unsigned char)str[i]);
+			next = 0;
+		}
+
+		else
+			next = 0;
+	}
+
+	return (str);
