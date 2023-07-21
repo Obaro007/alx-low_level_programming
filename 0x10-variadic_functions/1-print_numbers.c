1a8950e341d4d26c016a3f78946070db858764ab
Obaro007 <emamuzoobaro@gmail.com>
2023-07-21 04:11:28 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -6,21 +6,22 @@
  * @separator: The string to be printed between numbers.
  * @n: The number of integers passed to the function.
  */
-int sum_them_all(const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...);
+
 {
 	va_list ap;
-	unsigned int i, sum = 0;
+	va_start (ap, n);
 
-	if (n == 0)
-		return (0);
 
-	va_start(ap, n);
+	for(unsigned int i = 0; i < n; i++)
+	{	
+		printf("%d", va_arg(ap, int));
 
-	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
+		if (seperator && i < n -1)
+			printf("%s", seperator);
+	}
+
+	printf("\n");
 
 	va_end(ap);
-
-	return (sum);
 }
-
