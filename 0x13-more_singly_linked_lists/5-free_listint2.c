6fe713f34abb4c36060234c1170303c20a144c41
obaro007 <emamuzoobaro@gmail.com>
2023-08-02 15:02:14 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint2 - free the linked list
+ * @head : pointer to list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *cursor;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		cursor = *head;
+		*head = (*head)->next;
+		free(cursor);
+	}
+	head = NULL;
+}
